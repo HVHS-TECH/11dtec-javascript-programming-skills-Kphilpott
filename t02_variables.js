6fbe9d5ddef8318ced79e3ba3442d3f4bdf14996
Kphilpott <23424kp@hvhs.school.nz>
2025-04-29 23:58:31 +0000

    0.0.1

---
+++ t02_variables.js
@@ -0,0 +1,52 @@
+
+/*******************************************
+this is the variables lesson (t02_variables.js) 
+*******************************************/
+
+console.log("luxon is a egg");
+
+
+    //variables
+var usermoney = 207;
+var username = "User";
+var age = 16;
+var year = 2025;
+
+
+
+/*******************************************
+ Main Code
+ ******************************************/
+ 
+ console.log("Hello",username,);
+ console.log("As of",year,"you are almost",age,"years old")
+ console.log("You have",usermoney,"dollars");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*******************************************
+ Functions
+ *******************************************/
+
+
+
+
+
+
+
+
+
+
