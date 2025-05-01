2552cefc5a82348085d5f814b17791dec3ee4c61
Kphilpott <23424kp@hvhs.school.nz>
2025-05-01 00:53:30 +0000

    May 1, 2025, 12:53 AM

--- t04_debugging.js
+++ t04_debugging.js
@@ -4,7 +4,7 @@ console.log("Task 4 - Debugging")
 var area = length + width;
 
 // calculate the perimeter
-var perimeter = 2  ,length, + 2 + width; 
+var perimeter = 2  ,length = 2 + width; 
 
 // Set up the size of the page.
 var length = 8;
@@ -17,6 +17,7 @@ console.log("The same piece of paper has a perimeter of " ,perimeter,)
 // Check, does this result look sensible
 if (perimeter > area){
     console.log("Yay, your area is larger, this is right for this example")
-}else{
+}
+else{
     console.log("Hmm, your perimeter is larger. It shouldn't be for this example")
 }
