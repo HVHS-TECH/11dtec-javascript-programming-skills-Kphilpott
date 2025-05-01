a7c80cf62a4bfb00d51797d790f7d870727b1e2f
Kphilpott <23424kp@hvhs.school.nz>
2025-05-01 00:39:40 +0000

    May 1, 2025, 12:39 AM

---
+++ t03_maths.js
@@ -0,0 +1,57 @@
+
+/*******************************************
+this is the javascript maths lesson (t03_maths.js) 
+*******************************************/
+
+console.log("luxon is a egg");
+
+
+    //variables
+var usermoney = 207;
+var username = "User";
+var age = 16;
+var year = 2025;
+
+
+
+/*******************************************
+ Main Code
+ ******************************************/
+ 
+ console.log("Hello",username,);
+ console.log("As of",year,"you are almost",age,"years old")
+ console.log("You have",usermoney,"dollars");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*******************************************
+ Functions
+ *******************************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
