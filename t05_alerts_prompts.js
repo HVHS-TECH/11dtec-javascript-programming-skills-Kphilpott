3c1e31cc108bd65decba94960f866714b5b1c100
Kphilpott <23424kp@hvhs.school.nz>
2025-05-02 02:16:23 +0000

    May 2, 2025, 2:16 AM

---
+++ t05_alerts_prompts.js
@@ -0,0 +1,60 @@
+
+/*******************************************
+this is the alerts lesson (t05_alerts_prompts.js) 
+*******************************************/
+
+console.log("luxon is a egg");
+
+
+    //variables
+var usermoney = 207;
+var username = "User";
+var age = 16;
+var year = 2025;
+
+
+
+/*******************************************
+ Main Code
+ ******************************************/
+ 
+ console.log("Hello",username,);
+ console.log("As of",year,"you are almost",age,"years old")
+ console.log("You were born in" ,year - age)
+ console.log("In 10 years you will be:" ,age + 10)
+ console.log("You have",usermoney,"dollars");
+ console.log("Half of your money is" ,usermoney / 2, "Dollars")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*******************************************
+ Functions
+ *******************************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
