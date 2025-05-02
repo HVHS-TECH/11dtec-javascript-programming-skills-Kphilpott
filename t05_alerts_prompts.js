026490e163f7d57d9778cc0489e901ed33c0899e
Kphilpott <23424kp@hvhs.school.nz>
2025-05-02 02:27:30 +0000

    May 2, 2025, 2:27 AM

--- t05_alerts_prompts.js
+++ t05_alerts_prompts.js
@@ -31,7 +31,7 @@ var year = 2025;
  console.log("Hello",username,);
  console.log("As of",year,"you are almost",age,"years old")
  console.log("You were born in" ,year - age)
- console.log("In 10 years you will be:" ,age + 10)
+ console.log("In 10 years you will be:",age + 10,"years old")
  console.log("You have",usermoney,"dollars");
  console.log("Half of your money is" ,usermoney / 2, "Dollars")
 
