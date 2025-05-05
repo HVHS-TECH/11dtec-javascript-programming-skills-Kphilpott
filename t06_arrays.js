b3bb0e1fb4a2e66cfe7efb0649ca95d4cd7fbc58
Kphilpott <23424kp@hvhs.school.nz>
2025-05-05 21:09:31 +0000

    May 5, 2025, 9:09 PM

---
+++ t06_arrays.js
@@ -0,0 +1,69 @@
+
+/*******************************************
+this is the 1st arrays lesson (t06_arrays.js) 
+*******************************************/
+
+console.log("luxon is a egg");
+
+
+    //variables
+var usermoney = 207;
+var username = prompt("Welcome to my program! \nWhat is your name?", "User");
+var age = prompt("Please enter your age " + username, 16);
+var year = prompt("What year is it?", 2025);
+
+
+
+/*******************************************
+ Main Code
+ ******************************************/
+ 
+
+
+
+
+
+
+
+
+
+ console.log("Hello",username,);
+ console.log("As of",year,"you are almost",age,"years old")
+ console.log("You were born in" ,year - age)
+ console.log("In 10 years you will be:", parseInt(age) + 10, "years old");
+ console.log("You have",usermoney,"dollars");
+ console.log("Half of your money is" ,usermoney / 2, "Dollars")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*******************************************
+ Functions
+ *******************************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
