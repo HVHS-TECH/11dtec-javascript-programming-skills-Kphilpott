dec3a02e1a75035a533334d48562d963ecedf96c
Kphilpott <23424kp@hvhs.school.nz>
2025-05-05 21:32:49 +0000

    May 5, 2025, 9:32 PM

--- t06_arrays.js
+++ t06_arrays.js
@@ -13,6 +13,8 @@ var year = prompt("What year is it?", 2025);
 var usermoney = prompt("How much money do you have? \n(Do not put a Comma please)", 0);
 let chocolate = ["You dont like chocolate.", "its meh", "you like chocolate", "you like chocolate a lot"];
 let choice = prompt("Do you like chocolate from 0-3");
+let space = ["Good Choice.", "Bad Choice, have fun on Venus without a Spacesuit."];
+let choice2 = prompt("Do Like Space? \n0 for yes, 1 for no");
 /*******************************************
  Main Code
  ******************************************/
@@ -32,7 +34,8 @@ let choice = prompt("Do you like chocolate from 0-3");
  console.log("In 10 years you will be:", parseInt(age) + 10, "years old");
  console.log("You have",usermoney ,"dollars");
  console.log("Half of your money is" ,usermoney / 2, "Dollars")
-console.log(chocolate[choice] )
+ console.log(chocolate[choice] )
+ console.log(space[choice2])
 
 
 
