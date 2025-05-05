dd2dc1e6140bba2f8ba8e93cade54a149ac15b52
Kphilpott <23424kp@hvhs.school.nz>
2025-05-05 21:23:44 +0000

    May 5, 2025, 9:23 PM

--- t06_arrays.js
+++ t06_arrays.js
@@ -7,10 +7,10 @@ console.log("luxon is a egg");
 
 
     //variables
-var usermoney = 207;
 var username = prompt("Welcome to my program! \nWhat is your name?", "User");
 var age = prompt("Please enter your age " + username, 16);
 var year = prompt("What year is it?", 2025);
+var usermoney = prompt("How much money do you have?", 0);
 let chocolate = ["You dont like chocolate.", "its meh", "you like chocolate", "you like chocolate a lot"];
 let choice = prompt("Do you like chocolate from 0-3");
 alert(chocolate[choice]);
